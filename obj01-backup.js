28b4dfaecf9c0cf7550c22a214b1a9269caf6de2
Sacha Sedriks <sachased@gmail.com>
2020-02-11 14:20:03 -0800

    due to issues with brennan getting latest obj01.js, created backup file.

---
+++ obj01-backup.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/*
+
+// OBJECT 01 EXERCISE
+
+const loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return (this.water / this.flour) * 100;
+  }
+}
+
+console.log(loaf);
+console.log(loaf.hydration());
+
+
+// OBJECT 02 EXERCISE
+
+const obj = {
+  foo: 'chocolate',
+  bar: 'wood',
+  fum: 'fie',
+  quux: 'fluxx',
+  spam: 'gross'
+}
+
+for (const key in obj) {
+  console.log(`${key}:${obj[key]}`);
+}
+*/
+
+// OBJECT 03  EXERCISE
+
+const foodObj = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+}
+
+console.log(foodObj.meals[3]); // !!!!!!!!
