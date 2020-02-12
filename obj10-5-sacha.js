9c5b8372ce16044e77a1fe73c7c629d2938e34e3
Sacha Sedriks <sachased@gmail.com>
2020-02-11 17:17:35 -0800

    changed file name from obj02-sacha.js -> obj10-5-sacha.js & finished drill 10.5

---
+++ obj10-5-sacha.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// Sacha: doing the remaining drills 5—8 alone
+// from https://courses.thinkful.com/ei-js-v1/checkpoint/10
+
+///////////////////////////////////
+// 5. Properties that aren't there
+///////////////////////////////////
+
+const humanResources = [
+  {
+    name: 'Brad',
+    jobTitle: 'Intern',
+    boss: 'Tito'
+  },
+  {
+    name: 'Jess',
+    jobTitle: 'Senior Software Architect',
+    boss: 'Teresa'
+  },
+  {
+    name: 'Sacha',
+    jobTitle: 'Jr. Developer',
+    boss: 'Tito'
+  },
+  {
+    name: 'Teresa',
+    jobTitle: 'CEO'
+  },
+  {
+    name: 'Tito',
+    jobTitle: 'Developer',
+    boss: 'Jess'
+  }
+
+]; 
+
+for (const key in humanResources) {
+  //console.log(humanResources[key]);
+  if ('boss' in humanResources[key]) {
+    console.log(`${humanResources[key].jobTitle} ${humanResources[key].name} reports to ${humanResources[key].boss}`);
+  } else {
+    console.log(`${humanResources[key].jobTitle} ${humanResources[key].name} reports to no one, because s/he is the boss!`);
+  }
+}
